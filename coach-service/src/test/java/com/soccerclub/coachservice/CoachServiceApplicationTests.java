8fff7da180b37920e37662a37e51bf88e53ef67d
hanenchemkhi <hanen.chemkhi@gmail.com>
2023-04-24 11:47:18 -0400

    Initial commit

---
+++ coach-service/src/test/java/com/soccerclub/coachservice/CoachServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soccerclub.coachservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoachServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
