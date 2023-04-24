3200245f0bcde19d5aaf8cea30db5a1f1e3ddc99
hanenchemkhi <hanen.chemkhi@gmail.com>
2023-04-24 18:13:19 -0400

    Added Api gateway, discovery and config server modules

---
+++ team-service/src/main/java/com/soccerclub/teamservice/model/team.java
@@ -0,0 +1,12 @@
+package com.soccerclub.teamservice.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class team {
+    int id;
+    String name;
+
+
+    List<Player> players = new ArrayList<>();
+}
